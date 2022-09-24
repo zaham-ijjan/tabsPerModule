fc2beef4b556c8a8e14257db6fe131caa2074ecd
GitHub Action <action@github.com>
2022-09-24 22:42:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/zahamijjan/tabspermodule/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/zahamijjan/tabspermodule/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zahamijjan.tabspermodule.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.zahamijjan.tabspermodule.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
